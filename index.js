82a31f85384628c895e48e3afa78d06a8630465d
wadad zein <wadad_zein@hotmail.com>
2021-04-08 20:55:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
-const num5= 20;
-
 const num1= 31;
 const num2= 2;
-const multiply = num1 * num2;
+const multiply= num1 * num2;
 
-const random= Math.floor(Math.random()*10)+1;
 
-const num3=12;
-const num4=8;
+const random= Math.floor(Math.random()*10) +1;
+
+const num3= 12;
+const num4= 8;
 const mod= num3 % num4;
 
-const max= Math.max(num4, num2, num5);
+const num5= 20;
+const max= Math.max(num4, num3, num5);
